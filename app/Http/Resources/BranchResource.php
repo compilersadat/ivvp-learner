8da75d6a9a7933f419dc218725e7c871def53686
Sadat Hussain <sadcompiler@gmail.com>
2022-09-25 13:01:53 +0530

    content

--- app/Http/Resources/BranchResource.php
+++ app/Http/Resources/BranchResource.php
@@ -19,7 +19,21 @@ class BranchResource extends JsonResource
             'id'=>$this->id,
             'branch_id'=>$this->branch_id,
             'name'=>$this->name,
-            'content'=>ContentResource::collection(Content::where('branch',$this->branch_id)->groupBy('month')->get())
+            'content'=>[
+                "January"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","1")->get()),
+                "Febuary"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","2")->get()),
+                "March"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","3")->get()),
+                "April"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","4")->get()),
+                "May"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","5")->get()),
+                "June"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","6")->get()),
+                "July"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","7")->get()),
+                "August"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","8")->get()),
+                "September"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","9")->get()),
+                "October"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","10")->get()),
+                "November"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","11")->get()),
+                "December"=>ContentResource::collection(Content::where('branch',$this->branch_id)->where("month","12")->get())
+
+            ]
         ];
     }
 }
