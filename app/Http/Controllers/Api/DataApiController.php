bd4f78adff4b4e6a302f5b0a7741fb37791dfa0d
Sadat Hussain <sadcompiler@gmail.com>
2022-09-25 15:09:44 +0530

    data

--- app/Http/Controllers/Api/DataApiController.php
+++ app/Http/Controllers/Api/DataApiController.php
@@ -9,13 +9,15 @@ use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Api\ResponseController as ResponseController;
 use App\Models\Faculty;
 use App\Http\Resources\StudyMaterial;
+use App\Http\Resources\PackageResource;
+use App\Models\Package;
 class DataApiController extends ResponseController
 {
     public function homeData(){
 
         $slider=Slider::get();
          $data['sliders']=$slider;
-         $data['subscriptions']=[];
+         $data['subscriptions']=PackageResource::collection(Package::all());
          $data['study_materials']=StudyMaterial::collection(Faculty::all());
         $success['message'] = "Here is data";
         $success['data']=$data;
