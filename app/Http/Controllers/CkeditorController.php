ac39db6cd176956460fd39a991bc9ed76b600d6d
sadat <sadcompiler@gmail.com>
2023-01-26 17:22:29 +0530

    ckeditor

--- app/Http/Controllers/CkeditorController.php
+++ app/Http/Controllers/CkeditorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CkeditorController extends Controller
 {
@@ -21,15 +22,16 @@ class CkeditorController extends Controller
     public function upload(Request $request)
     {
         if($request->hasFile('upload')) {
-            $originName = $request->file('upload')->getClientOriginalName();
-            $fileName = pathinfo($originName, PATHINFO_FILENAME);
-            $extension = $request->file('upload')->getClientOriginalExtension();
-            $fileName = $fileName.'_'.time().'.'.$extension;
+            // $originName = $request->file('upload')->getClientOriginalName();
+            // $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            // $extension = $request->file('upload')->getClientOriginalExtension();
+            // $fileName = $fileName.'_'.time().'.'.$extension;
 
-            $request->file('upload')->move(public_path('images'), $fileName);
+            // $request->file('upload')->move(public_path('images'), $fileName);
+            $path = Storage::disk('s3')->put('questions', $request->upload);
 
             $CKEditorFuncNum = $request->input('CKEditorFuncNum');
-            $url = asset('images/'.$fileName); 
+            $url = env('S3_STORAGE_BASE_URL').$path; 
             $msg = 'Image uploaded successfully'; 
             $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
 
