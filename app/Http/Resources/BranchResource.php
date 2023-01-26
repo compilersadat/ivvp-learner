10d55089365193819b1b94e6dc72607ce25dd8b7
sadat <sadcompiler@gmail.com>
2023-01-26 20:46:58 +0530

    month name

--- app/Http/Resources/BranchResource.php
+++ app/Http/Resources/BranchResource.php
@@ -15,6 +15,7 @@ class BranchResource extends JsonResource
      */
     public function toArray($request)
     {
+        
         return [
             'id'=>$this->id,
             'branch_id'=>$this->branch_id,
--- app/Http/Resources/ExamResource.php
+++ app/Http/Resources/ExamResource.php
@@ -15,12 +15,26 @@ class ExamResource extends JsonResource
      */
     public function toArray($request)
     {
+        $months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+            ];
         return [
             'id'=>$this->id,
             'title'=>$this->title,
             'total_questions'=>$this->no_questions,
             'marks'=>$this->marks,
-            'month'=>$this->month,
+            'month'=>$months[$this->month-1],
             'questions'=>QuestionResource::collection(Question::where('exam_id',$this->id)->get()),
             'is_active'=>$this->is_published==1
         ];
