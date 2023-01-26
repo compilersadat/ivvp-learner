743ca02682fde93bc5babbdeb79f01fa8bbe6438
sadat <sadcompiler@gmail.com>
2023-01-26 12:57:28 +0530

    fix

--- app/Http/Resources/ContentResource.php
+++ app/Http/Resources/ContentResource.php
@@ -18,7 +18,7 @@ class ContentResource extends JsonResource
             "title"=>$this->title,
             "description"=>$this->description,
             "type"=>$this->type,
-            "file_url"=>S3upload::where('id',$this->file_url)->first()->value("url"),
+            "file_url"=>S3upload::where('id',$this->file_url)->first()->value("id"),
             "thumbnail"=>env('S3_STORAGE_BASE_URL').$this->thumbnail
 
         ];
