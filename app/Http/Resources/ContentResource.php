6c92ec250c7f30ac9774a9c6a2c23d698c94ff8e
compilersadat <sadcompiler@gmail.com>
2023-02-05 12:45:49 +0530

    added month in content

--- app/Http/Resources/ContentResource.php
+++ app/Http/Resources/ContentResource.php
@@ -14,14 +14,27 @@ class ContentResource extends JsonResource
      */
     public function toArray($request)
     {
+        $months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+            ];
         return [
             "title"=>$this->title,
             "description"=>$this->description,
             "type"=>$this->type,
-            // "file_url"=>$this->file_url,
             "file_url"=>S3upload::where('id',$this->file_url)->value("url"),
-            "thumbnail"=>env('S3_STORAGE_BASE_URL').$this->thumbnail
-
+            "thumbnail"=>env('S3_STORAGE_BASE_URL').$this->thumbnail,
+            "month"=>$months[$this->month]
         ];
     }
 }
