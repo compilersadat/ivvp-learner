7e0a58a0ce031a2e4f0a0f3914654cece71c49a4
compilersadat <sadcompiler@gmail.com>
2024-02-17 12:22:53 +0530

    new test routes

--- routes/web.php
+++ routes/web.php
@@ -118,13 +118,19 @@ Route::prefix('admin')->group(function () {
     Route::get('test/publish/{id}',[TestSeriesTestController::class,'changeStatus'])->name('testseries.test.changeStatus');
     Route::get('test/show/{id}',[TestSeriesTestController::class,'show'])->name('testseries.test.show');
    
-    Route::get('questions/create',[TestSeriesQuestionController::class,'create'])->name('test.questions.create');
-    Route::post('questions/store',[TestSeriesQuestionController::class,'store'])->name('test.questions.store');
-    Route::get('questions/delete/{id}',[TestSeriesQuestionController::class,'delete'])->name('test.questions.delete');
-    Route::get('questions/edit/{id}',[TestSeriesQuestionController::class,'edit'])->name('test.questions.edit');
-    Route::post('questions/update/{id}',[TestSeriesQuestionController::class,'update'])->name('test.questions.update');
-    Route::get('questions/show/{id}',[TestSeriesQuestionController::class,'show'])->name('test.questions.show');
+    Route::get('test/questions/create',[TestSeriesQuestionController::class,'create'])->name('test.questions.create');
+    Route::post('test/questions/store',[TestSeriesQuestionController::class,'store'])->name('test.questions.store');
+    Route::get('test/questions/delete/{id}',[TestSeriesQuestionController::class,'delete'])->name('test.questions.delete');
+    Route::get('test/questions/edit/{id}',[TestSeriesQuestionController::class,'edit'])->name('test.questions.edit');
+    Route::post('test/questions/update/{id}',[TestSeriesQuestionController::class,'update'])->name('test.questions.update');
+    Route::get('test/questions/show/{id}',[TestSeriesQuestionController::class,'show'])->name('test.questions.show');
 
+    Route::get('questions/create',[QuestionController::class,'create'])->name('questions.create');
+    Route::post('questions/store',[QuestionController::class,'store'])->name('questions.store');
+    Route::get('questions/delete/{id}',[QuestionController::class,'delete'])->name('questions.delete');
+    Route::get('questions/edit/{id}',[QuestionController::class,'edit'])->name('questions.edit');
+    Route::post('questions/update/{id}',[QuestionController::class,'update'])->name('questions.update');
+    Route::get('questions/show/{id}',[QuestionController::class,'show'])->name('questions.show');
     
 
 });
