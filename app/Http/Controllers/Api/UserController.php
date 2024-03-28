d811289883453e9ac693c489f04469a21afe07fd
compilersadat <sadcompiler@gmail.com>
2024-03-28 10:32:01 +0530

    phonepe_intigration

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\StudentPackage;
 use Razorpay\Api\Api;
 use App\Models\Transaction;
+use Ixudra\Curl\Facades\Curl;
+
 
 class UserController extends ResponseController
 {
@@ -88,18 +90,15 @@ class UserController extends ResponseController
         if($validator->fails()){
             return $this->sendError($validator->errors());
         }
-        $api = new Api(env("RAZOR_PAY_KEY"), env("RAZOR_PAY_SECRETE"));
-        $response=$api->order->create(array('receipt' => md5(mt_rand()), 'amount' => $request->price*100, 'currency' => 'INR'));
-        if($response){
+        
             $transaction=new Transaction();
             $transaction->student_id=$request->user()->id;
             $transaction->package_id=$request->package_id;
             $transaction->package_name=$request->package_name;
             $transaction->number_of_months=$request->number_of_month;
             $transaction->price=$request->price;
-            $transaction->order_id=$response['id'];
-            $transaction->reciept=$response['receipt'];
-            $transaction->status=$response['status'];
+            $transaction->order_id=$request->merch_order_id;
+            $transaction->status='ordered';
             if($transaction->save()){
                 $responses['message']="Order Created.";
                 $responses['subscription_details']=$transaction;
@@ -108,43 +107,69 @@ class UserController extends ResponseController
                 $error = "Sorry! Please try again";
                 return $this->sendError($error, 401);
             }
-        }
+        
 }
 
 public function updatePackage(Request $request){
     $validator = Validator::make($request->all(), [
         'order_id'=>'required',
-       'transaction_id'=>'required'
     ]);
     if($validator->fails()){
         return $this->sendError($validator->errors());
     }
-    $transaction=Transaction::where('order_id',$request->order_id)->first();
 
-    $transaction->status="compeleted";
-    $transaction->transaction_id=$request->transaction_id;
-    $transaction->update();
-    $count=StudentPackage::where('student_id',$request->user()->id)->count();
-        $student_package=new StudentPackage();
-        if($count){
-            $student_package=StudentPackage::where('student_id',$request->user()->id)->first();
-        }
-        $student_package->student_id=$transaction->student_id;
-        $student_package->package_id=$transaction->package_id;
-        $student_package->package_name=$transaction->package_name;
-        $student_package->number_of_months=$transaction->number_of_months;
-        $student_package->price=$transaction->price;
-        $student_package->start_date=date('d-m-y');
-        $student_package->start_month=date('m');
-        $student_package->status=2;
-        $student_package->payment_status="compeleted";
-        if($count==0?$student_package->save():$student_package->update()){
-            $response['message']="Subscription Activated";
-            $response['subscription_details']=$student_package;
-            return $this->sendResponse($response);
+    $transaction=Transaction::where('order_id',$request->order_id)->first();
+    $saltKey = env('PHONE_PE_SALT_KEY_DEV');
+    $saltIndex = env('PHONE_PE_SALT_INDEX_DEV');
+    $merchant_id = env('PHONE_PE_MERCHANTID_DEV');
+
+        $finalXHeader = hash('sha256','/pg/v1/status/'.$merchant_id.'/'.$request->order_id.$saltKey).'###'.$saltIndex;
+
+        $response_encoded = Curl::to('https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/'.$merchant_id.'/'.$request->order_id)
+                ->withHeader('Content-Type:application/json')
+                ->withHeader('accept:application/json')
+                ->withHeader('X-VERIFY:'.$finalXHeader)
+                ->withHeader('X-MERCHANT-ID:'.$request->order_id)
+                ->get();
+        $response = json_decode($response_encoded);
+        if($response->success){
+                $transaction->status="compeleted";
+                $transaction->transaction_id=$response->data->transaction_id;
+                $transaction->update();
+                $count=StudentPackage::where('student_id',$request->user()->id)->count();
+                    $student_package=new StudentPackage();
+                    if($count){
+                        $student_package=StudentPackage::where('student_id',$request->user()->id)->first();
+                    }
+                    $student_package->student_id=$transaction->student_id;
+                    $student_package->package_id=$transaction->package_id;
+                    $student_package->package_name=$transaction->package_name;
+                    $student_package->number_of_months=$transaction->number_of_months;
+                    $student_package->price=$transaction->price;
+                    $student_package->start_date=date('d-m-y');
+                    $student_package->start_month=date('m');
+                    $student_package->status=2;
+                    $student_package->payment_status="compeleted";
+                    if($count==0?$student_package->save():$student_package->update()){
+                        $response['message']="Subscription Activated";
+                        $response['subscription_details']=$student_package;
+                        return $this->sendResponse($response);
+                    }else{
+                        $error = "Sorry! Please try again";
+                        return $this->sendError($error, 401);
+                    }
+            
         }else{
-            $error = "Sorry! Please try again";
-            return $this->sendError($error, 401);
+            if($response == 'PAYMENT_PENDING'){
+                $transaction->status="pending";
+                $transaction->update();
+            }
+            $error = $response->message;
+            if($response == 'PAYMENT_ERROR'){
+                $error .= ' '.$response->data->responseCodeDescription;
+            }
+            return $this->sendError($error, 500);
         }
+    
 }
 }
