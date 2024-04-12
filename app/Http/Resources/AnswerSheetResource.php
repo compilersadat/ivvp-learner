221ab25d69f6062a7c4880f4ecfe7b1e81beafdc
compilersadat <sadcompiler@gmail.com>
2024-04-12 15:46:09 +0530

    fixed

--- app/Http/Resources/AnswerSheetResource.php
+++ app/Http/Resources/AnswerSheetResource.php
@@ -10,12 +10,11 @@ class AnswerSheetResource extends JsonResource
 {
     public function toArray($request)
     {
-        request()->add(['attempt'=>$this->id]);
         $marks=TestSeriesStudentAnswer::where('exam_id',$this->exam_id)->where('student_id',$request->user()->id)->where('attempt',$this->id)->sum('');
 
         return[
          'id' => $this->id,
-         "questions" => QuestionWithAnswerResource::collection(TestSeriesQuestion::where('test_id',$this->test_id)->get()),
+         "questions" => QuestionWithAnswerResource::collection(TestSeriesQuestion::where('test_id',$this->test_id)->get())->attempt($this->id),
          "date" => date('d-m-yyyy',$this->created_at),
         ];
     }
