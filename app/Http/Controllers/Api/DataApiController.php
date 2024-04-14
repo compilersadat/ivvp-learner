8c8a0cd6f82f0ff2ed52dab23283f1d571380054
compilersadat <sadcompiler@gmail.com>
2024-04-14 14:46:46 +0530

    fix

--- app/Http/Controllers/Api/DataApiController.php
+++ app/Http/Controllers/Api/DataApiController.php
@@ -39,9 +39,9 @@ class DataApiController extends ResponseController
                         if($st_package->status==2){
                             $package = Package::where('id',$st_package->package_id)->first();
                             array_merge($month_range,$this->calculateRangeOfMonths($package->month,$st_package->number_of_months));
-                            return $this->sendResponse($this->calculateRangeOfMonths($package->month,$st_package->number_of_months));
                         }
                     }
+                    return $this->sendResponse($month_range);
 
                     $prime_content=Content::where('branch',$student->branch)->where('year',$student->year)->whereIn('month',$month_range)->get();
                     $current_month_videos=Content::where('branch',$student->branch)->where('year',$student->year)->where('month',date('n'))->where('type','video_lecture')->get();
