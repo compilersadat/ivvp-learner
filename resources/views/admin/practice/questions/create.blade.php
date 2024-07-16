ae1459ca24b9f2ba36a2b48ea1ab6cae1c41a79d
compilersadat <sadcompiler@gmail.com>
2024-07-16 19:01:27 +0530

    Update create.blade.php

--- resources/views/admin/practice/questions/create.blade.php
+++ resources/views/admin/practice/questions/create.blade.php
@@ -22,7 +22,7 @@
         @include('partials.alerts')
       <div class="card-header"><strong>ADD NEW QUESTION</strong></div>
         <div class="card-body card-block">
-            <form method="POST" action="{{route('test.questions.store')}}" enctype="multipart/form-data">
+            <form method="POST" action="{{route('practice.questions.store')}}" enctype="multipart/form-data">
                 @csrf
 
         <div class="form-group">
