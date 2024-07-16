24db073a17e62d1283b52138695bbac47ee5d3ab
compilersadat <sadcompiler@gmail.com>
2024-07-16 14:56:08 +0530

    Update index.blade.php

--- resources/views/admin/practice/questions/index.blade.php
+++ resources/views/admin/practice/questions/index.blade.php
@@ -59,4 +59,19 @@
         </table>
     </div>
 </div>
+<script src="{{ asset('js/lib/data-table/datatables.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/dataTables.bootstrap.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/dataTables.buttons.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/buttons.bootstrap.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/jszip.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/vfs_fonts.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/buttons.html5.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/buttons.print.min.js')}}"></script>
+<script src="{{ asset('js/lib/data-table/buttons.colVis.min.js')}}"></script>
+<script src="{{ asset('js/init/datatables-init.js')}}"></script>
+     <script type="text/javascript">
+    $(document).ready(function() {
+      $('#bootstrap-data-table-export').DataTable();
+  } );
+</script>
 @endsection
