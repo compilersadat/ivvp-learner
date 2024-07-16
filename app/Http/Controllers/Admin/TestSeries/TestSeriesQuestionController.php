43d48fa555d3ec23f4800bc64c8cf75dddd4e8b2
compilersadat <sadcompiler@gmail.com>
2024-07-16 13:38:38 +0530

    Update TestSeriesQuestionController.php

--- app/Http/Controllers/Admin/TestSeries/TestSeriesQuestionController.php
+++ app/Http/Controllers/Admin/TestSeries/TestSeriesQuestionController.php
@@ -99,6 +99,7 @@ class TestSeriesQuestionController extends Controller
     {
         $content=TestSeriesQuestion::where('id', $id)->first();
         $test = TestSeriesTest::where('id',$content->test_id)->get();
+        return $test;
         $sections= TestSeriesSection::where('test_series_id',$test->testseries_id)->get();
         return view('admin.test_series_test.edit_questions', compact('content','test','sections'));
     }
