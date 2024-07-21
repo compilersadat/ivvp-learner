fbbb41a2d5c88a1f35b638d1cd98e3895e9a8514
compilersadat <sadcompiler@gmail.com>
2024-07-21 14:24:45 +0530

    fix

--- app/Http/Controllers/Api/TestSeriesController.php
+++ app/Http/Controllers/Api/TestSeriesController.php
@@ -36,7 +36,7 @@ class TestSeriesController extends ResponseController
                 $answerDb->attempt=$attempt->id;
                 $answerDb->save();
             }
-            $student_attempt=TestSeriesStudentAttempt::where('id',$attempt->id);
+            $student_attempt=TestSeriesStudentAttempt::where('attempt', $attempt->attempt);
             $success['data'] = new AnswerSheetResource($student_attempt);
             $success['message'] = "Exam Submitted.";
             return $this->sendResponse($success);   
