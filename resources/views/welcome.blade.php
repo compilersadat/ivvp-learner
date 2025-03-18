00bd1f5ff56c7a5de47816e32ca88a1dddc45c47
compilersadat <sadcompiler@gmail.com>
2025-03-18 14:57:29 +0530

    account deletion url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>IVP BOOK</title>
+        <title>iVVP Innovativeview publication</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    IVP BOOK
+                    iVVP
                 </div>
 
                 <!-- <div class="links">
