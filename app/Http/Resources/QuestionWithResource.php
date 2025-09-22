2dfc1de9e185e3360813423a19dec267533593be
compilersadat <sadcompiler@gmail.com>
2025-09-22 13:48:56 +0530

    Update QuestionWithResource.php

--- app/Http/Resources/QuestionWithResource.php
+++ app/Http/Resources/QuestionWithResource.php
@@ -13,21 +13,33 @@ class QuestionWithResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
 
-    public function toArray($request)
-    {
-        $ABCD=["A","B","C","D"];
-        $student_answer=StudentAnswer::where('exam_id',$this->exam_id)->where('student_id',$request->user()->id)->where('question_id',$this->id)->first();
-
-        return [
-            'id'=>$this->id,
-            'question'=>$this->question,
-            'option1'=>$this->option1,
-            'option2'=>$this->option2,
-            'option3'=>$this->option3,
-            'option4'=>$this->option4,
-            'answer'=>$this->answer,
-            'student_answer'=>$ABCD[$student_answer->answer-1],
-            'is_right'=>$this->answer==$student_answer->answer
-        ];
-    }
+     public function toArray($request)
+     {
+         $ABCD = ["A","B","C","D"];
+         $student_answer = StudentAnswer::where('exam_id',$this->exam_id)
+             ->where('student_id',$request->user()->id)
+             ->where('question_id',$this->id)
+             ->first();
+     
+         $studentAnswerValue = null;
+         $isRight = false;
+     
+         if ($student_answer && $student_answer->answer > 0 && $student_answer->answer <= count($ABCD)) {
+             $studentAnswerValue = $ABCD[$student_answer->answer - 1];
+             $isRight = $this->answer == $student_answer->answer;
+         }
+     
+         return [
+             'id'             => $this->id,
+             'question'       => $this->question,
+             'option1'        => $this->option1,
+             'option2'        => $this->option2,
+             'option3'        => $this->option3,
+             'option4'        => $this->option4,
+             'answer'         => $this->answer,
+             'student_answer' => $studentAnswerValue, // will be null if no valid answer
+             'is_right'       => $isRight
+         ];
+     }
+     
 }
